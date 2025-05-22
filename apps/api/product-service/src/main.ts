2d48c23cd12b41252416b2461c904940e027efb8
pdelagrange <pierre.delagrange@hotmail.fr>
2025-05-22 14:52:46 +0200

    fix(api): env for nats server

--- apps/api/product-service/src/main.ts
+++ apps/api/product-service/src/main.ts
@@ -13,10 +13,12 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.NATS,
     options: {
-      servers: ['nats://localhost:4222'],
+      servers: [process.env.NATS_URL ?? 'nats://localhost:4222'],
     },
   });
-  app.listen().then(() => logger.log("Microservice Product is listening")).catch(() => logger.error("Microservice Product is not listening"));
+  app.listen()
+      .then(() => logger.log("Microservice Product is listening"))
+      .catch((error) => logger.error("Microservice Product is not listening ", error, "Bootstrap"));
 }
 
 void bootstrap();
