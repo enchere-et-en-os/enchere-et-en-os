d0d6f20fa89a2e771f5749edfe9585f967ef8d1e
AkiHir0o <guillaume.grandvoinet@gmail.com>
2025-05-22 16:49:37 +0200

    feat(websocket): init websocket, prettier config

--- apps/api/api-gateway/src/app.controller.ts
+++ apps/api/api-gateway/src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { AppService} from './app.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  
-  @Get("/ping-services")
+  @Get('/ping-services')
   pingServices() {
     return this.appService.pingServices();
   }
--- packages/eslint/base.js
+++ packages/eslint/base.js
@@ -13,43 +13,45 @@ import unusedImports from './configs/unused-imports.js';
 import jsonConfig from './configs/json.js';
 import stylisticConfig from './configs/stylistic.js';
 import perfectionistConfig from './configs/perfectionist.js';
+import prettierConfig from "./configs/prettier.js";
 
 export default tseslint.config(
-  ...jsonConfig,
-  ...baseConfig,
-  ...typescriptConfig,
-  ...jsdocConfig,
-  ...sonarjsConfig,
-  ...unicornConfig,
-  ...promiseConfig,
-  ...unusedImports,
-  ...stylisticConfig,
-  ...perfectionistConfig,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
+    ...jsonConfig,
+    ...baseConfig,
+    ...typescriptConfig,
+    ...jsdocConfig,
+    ...sonarjsConfig,
+    ...unicornConfig,
+    ...promiseConfig,
+    ...unusedImports,
+    ...stylisticConfig,
+    ...perfectionistConfig,
+    ...prettierConfig,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
 
-      ecmaVersion: 5,
-      sourceType: 'commonjs',
+            ecmaVersion: 5,
+            sourceType: 'commonjs',
 
-      parserOptions: {
-        project: true,
-      },
+            parserOptions: {
+                project: true,
+            },
+        },
+
+        rules: {
+            '@typescript-eslint/no-invalid-this': 'off', // why?
+            '@typescript-eslint/quotes': 'off', // why?
+            'sonarjs/function-return-type': 'off',
+        },
+        ignores: [
+            '**/dist/',
+            '**/build/',
+            '**/logs/',
+            'tsconfig.**',
+            '**/eslint.config.mjs',
+        ],
     },
-
-    rules: {
-      '@typescript-eslint/no-invalid-this': 'off', // why?
-      '@typescript-eslint/quotes': 'off', // why?
-      'sonarjs/function-return-type': 'off',
-    },
-    ignores: [
-      '**/dist/',
-      '**/build/',
-      '**/logs/',
-      'tsconfig.**',
-      '**/eslint.config.mjs',
-    ],
-  },
 );
