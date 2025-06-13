b8d07fe59ff83225407ddd4753e1ed0cdcc1e354
pdelagrange <pierre.delagrange@hotmail.fr>
2025-06-13 15:44:42 +0200

    feat: Valider les tokens via l'API gateway (AuthGuard) (#26)

--- apps/api/api-gateway/src/app.module.ts
+++ apps/api/api-gateway/src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  PolicyEnforcementMode,
+  TokenValidation,
+} from 'nest-keycloak-connect';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -19,8 +26,16 @@ import { AppService } from './app.service';
         },
       },
     ]),
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080', // might be http://localhost:8080/auth for older keycloak versions
+      realm: 'enchere',
+      clientId: 'front',
+      secret: 'uHyTIrI3y2c2UtuN09zrI7mzJSzfLJRH',
+      policyEnforcement: PolicyEnforcementMode.PERMISSIVE, // optional
+      tokenValidation: TokenValidation.ONLINE, // optional
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useValue: AuthGuard }],
 })
 export class AppModule {}
